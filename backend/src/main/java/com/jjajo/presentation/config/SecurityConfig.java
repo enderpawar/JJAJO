8eb9239b9954ec453784bf3705298b6141e655ae
enderpawar <enderpawar@naver.com>
2026-02-05 09:34:33 +0900

    fix: form login으로 로그인 시 오류가 발생하던 문제 수정정

--- backend/src/main/java/com/jjajo/presentation/config/SecurityConfig.java
+++ backend/src/main/java/com/jjajo/presentation/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
@@ -98,7 +97,7 @@ public class SecurityConfig {
                 .deleteCookies("JSESSIONID")
                 .invalidateHttpSession(true)
             )
-            .formLogin(Customizer.withDefaults());
+            .formLogin(form -> form.disable());
         debugLogFilter.ifPresent(f -> http.addFilterBefore(f, AuthorizationFilter.class));
 
         return http.build();
